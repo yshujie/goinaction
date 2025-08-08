0709f0f97d07ab93b3e1bc793ff879997f8ca3e8
yangshujie <yangshujie@fangcunyisheng.com>
2025-08-08 16:14:17 +0800

    feat 两种方式实现 slice 反转

--- slice/reverse-slice.go
+++ slice/reverse-slice.go
@@ -1,7 +1,7 @@
 package slice_test
 
 /**
-✅ 题目四：翻转一个切片（原地修改）
+✅ 题目：翻转一个切片（原地修改）
 写一个函数，反转一个 int 类型的切片
 输入: []int{1, 2, 3, 4}
 输出: []int{4, 3, 2, 1}
@@ -12,9 +12,17 @@ package slice_test
 - 时间复杂度 O(n)
 */
 
-func ReverseSlice(s []int) []int {
+func Reverse(s []int) []int {
 	for i := 0; i < len(s)/2; i++ {
 		s[i], s[len(s)-i-1] = s[len(s)-i-1], s[i]
+	}
+
+	return s
+}
+
+func Reverse2(s []int) []int {
+	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
+		s[i], s[j] = s[j], s[i]
 	}
 
 	return s
